83b0b2e4c6f10553cca0940d4da34400e518984c
Sydney Workman <sworkman1996@gmail.com>
2022-03-29 20:49:28 -0500

    button styling

--- src/Layout/Components/CreateDeckBtn.js
+++ src/Layout/Components/CreateDeckBtn.js
@@ -8,12 +8,11 @@ export default function CreateDeckBtn() {
         <Link to='/' className="btn btn-secondary my-2 w-30 h-5">
             <svg 
                 xmlns="http://www.w3.org/2000/svg"
-                className="mr-1"
                 width="16"
                 height="16"
                 stroke="currentColor"
                 strokeWidth={2}
-                class="bi bi-plus"
+                className="bi bi-plus mr-2"
                 viewBox="0 0 16 16"
             >
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
